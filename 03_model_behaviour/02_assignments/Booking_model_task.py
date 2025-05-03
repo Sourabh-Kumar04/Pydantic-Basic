506e476c15cd7c121f66d58d612903b4371c77eb
Sourabh-Kumar04 <sourabhkumar01010101@gmail.com>
2025-05-03 18:40:06 +0000

    feat: add booking model as assignment

---
+++ 03_model_behaviour/02_assignments/Booking_model_task.py
@@ -0,0 +1,9 @@
+from pydantic import BaseModel
+
+# TODO: Create Booking model
+# Fields:
+# - user_id: int
+# - room_id: int
+# - nights: int (must be greater than 0)
+# - rate_per_night: float (must be greater than 0)
+# - Also, add computed fields: total_amount = nights * rate_per_night
