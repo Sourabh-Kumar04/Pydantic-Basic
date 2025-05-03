0dc526ef06eb3462f7e04a1d1d0a6081ff7cc0ba
Sourabh-Kumar04 <sourabhkumar01010101@gmail.com>
2025-05-03 19:36:18 +0000

    feat: add Course model as assignment

---
+++ 04_nested_model/02_assignments/Course_model_aasignment.py
@@ -0,0 +1,8 @@
+from pydantic import BaseModel
+from typing import List
+
+# TODO: Create Course model
+# Each Course has modules
+# Ecah Module has lessons
+# Each Module has lessons
+# Each lesson has a title and a description
