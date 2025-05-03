57eb9d01af9de58dacb6d36dd5e1220de9270b62
Sourabh-Kumar04 <sourabhkumar01010101@gmail.com>
2025-05-03 17:17:39 +0000

    feat: add Employee model as assignment

---
+++ 02_fields/02_assignment/employee_model_starter.py
@@ -0,0 +1,7 @@
+from pydantic import BaseModel
+
+# TODO: Create Employee model with the following fields:
+# - id (int)
+# - name (str (min 3 characters))
+# - department (optional str (default = "General"))
+# - salary (float (must be greater than 10000))
